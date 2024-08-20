a022a8f115a6c72615263073615c95ffbe7e3e18
Lukas_leandro <leandrolukas916@gmail.com>
2024-08-20 20:18:01 -0300

    register business in admin

--- business/admin.py
+++ business/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from business import models
 
-# Register your models here.
+class BusinessAdmin(admin.ModelAdmin):
+    list_display = ('social_name', 'cnpj', 'is_active', 'created_at', 'updated_at')
+    search_fields = ('social_name', 'cnpj')
+    list_filter = ('is_active',)
+
+admin.site.register(models.Business, BusinessAdmin)
